cde1d11b153312293fc66520b788689fd8dd618b
Mihail Lovchitskiy <misha_aid@mail.ru>
2022-10-19 13:41:41 +0300

    updated master

--- app/src/main/java/com/example/lessonandroid/MainActivity.kt
+++ app/src/main/java/com/example/lessonandroid/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.lessonandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lessonandroid.databinding.ActivityMainBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val viewBinding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
+    private val fragmentContainerId = R.id.main_fragments_container
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
